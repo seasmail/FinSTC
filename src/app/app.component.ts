5118519dea1294c1897cf20d0afc08fb996b496c
Ksu <k.pyagay@unios.io>
2019-02-03 18:36:07 +0300

    Working app!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import {MatTable} from '@angular/material';
+
+export interface SomeObject {
+  position: number;
+  name: string;
+}
+
+const TABLE_1: SomeObject[] = [
+  {position: 1, name: 'Table'},
+  {position: 2, name: 'Cup'}
+];
+
+const TABLE_2: SomeObject[] = [
+  {position: 1, name: 'Table'},
+  {position: 2, name: 'Cup'},
+  {position: 3, name: 'Aim'},
+  {position: 4, name: 'Good'}
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Test';
+  displayedColumns: string[] = ['position', 'name'];
+  dataSource1 = TABLE_1;
+  dataSource2 = TABLE_2;
+  @ViewChild('t1') table: MatTable<SomeObject>;
+  @ViewChild('t2') table2: MatTable<SomeObject>;
+  public prevId;
+
+  onListDrop(event: CdkDragDrop<string[]>, dataSource: any, t: any) {
+     this.prevId = dataSource.findIndex((d) => d === event.item.data);
+    if (event.previousContainer === event.container) {
+      moveItemInArray(dataSource, this.prevId, event.currentIndex);
+      t.renderRows();
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousContainer.data.findIndex((d) => d === event.item.data),
+        event.currentIndex);
+      this.table.renderRows();
+      this.table2.renderRows();
+    }
+  }
+
 }
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material';
+import {CdkColumnDef, CdkTableModule} from '@angular/cdk/table';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DragDropModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    CdkTableModule
   ],
-  providers: [],
+  providers: [CdkColumnDef],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
